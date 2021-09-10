84f97787c9656a8b54daadbdc3cec44df0dc981d
maoakeEnterprise <maoake.terii@gmail.com>
2021-09-10 19:33:07 +0200

    Presque fin du tutoriel mais finally first screen

--- flutter_app_learning/lib/main.dart
+++ flutter_app_learning/lib/main.dart
@@ -15,12 +15,14 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Learning 1',
-      home: buildHomePage('Je suis le best'),
+      home: buildHomePage('Je suis le best', context),
+
     );
   }
 }
 
-Widget buildHomePage(String titlevar){
+Widget buildHomePage(String titlevar, BuildContext context){
+
 
   var stars = Row(
     mainAxisSize: MainAxisSize.min,
@@ -33,12 +35,75 @@ Widget buildHomePage(String titlevar){
     ],
   );
 
-  Widget tilesection = Container(
 
+  Widget titlesection = Container(
+    padding: const EdgeInsets.all(32),
+    child: Row(
+
+      children: [
+        Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: const Text(
+                      'OESHIN LAKE CAMPGROUND',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Text(
+                    'Kandersted Swtizerland',
+                  style: TextStyle(
+                    color: Colors.grey[500],
+                  ),
+                ),
+              ],
+            ),
+        ),
+        Icon(
+          Icons.star,
+          color: Colors.red[500],
+        ),
+        const Text('41'),
+      ],
+    ),
+  );
+
+  Widget buttons = Column(
+    crossAxisAlignment: CrossAxisAlignment.center,
+    children: [
+      Icon(
+          Icons.phone,
+          color: Colors.blue[500],
+      ),
+      Text('Yes'),
+    ],
   );
 
   Widget buttonsection = Container(
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        buttons,
+        buttons,
+        buttons,
+      ],
+    ),
+  );
 
+  Widget textsection = const Padding(
+    padding: EdgeInsets.all(32),
+    child: Text('Lorem ipsum dolor sit amet. Qui quod '
+        'iste qui veniam doloribus ea reiciendis atque '
+        'eum vitae debitis ea reiciendis dolores ut totam'
+        ' dolorem. Eos consequatur fuga hic omnis quia est '
+        'magnam incidunt qui minus omnis aut laborum possimus '
+        'repudi aperiaut ipsa aderunt rerum.',
+      softWrap: true,
+    ),
   );
 
   return Scaffold(
@@ -46,25 +111,17 @@ Widget buildHomePage(String titlevar){
       title: const Text('First Devoir'),
     ),
     body: Column(
-      children: [Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Column(
-            children: [
-              Image.asset(
-                'images/lake.jpg',
-
-                height: 240,
-                fit: BoxFit.cover,
-              ),
-              Text('tilesection '),//
-              Text('buttonsection'),
-              Text('Textsection'),
-            ],
-          ),
-        ],
-      ),]
+      children: [
+        Image.asset(
+          'assets/images/lake.jpg',
+          width: 600,
+          height: 240,
+          fit: BoxFit.cover,
+        ),
+        titlesection,
+        buttonsection,
+        textsection,
+      ],
     ),
   );
 }
